d2bb8a14716d3ac89f82a52f96c86fa089ef76d6
tomoko <tomo09ysd@gmail.com>
2025-08-26 19:11:53 +0900

    categoriesテーブル作成

--- src/database/migrations/2025_08_26_190603_create_contacts_table.php
+++ src/database/migrations/2025_08_26_190603_create_contacts_table.php
@@ -15,6 +15,15 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')  ->constrained('categories')  ->cascadeOnDelete();
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->tinyInteger('gender');     // 1:男性,2:女性,3:その他
+            $table->string('email', 255);
+            $table->string('tel', 255);
+            $table->string('address', 255);
+            $table->string('building', 255)->nullable();
+            $table->text('detail');
             $table->timestamps();
         });
     }
